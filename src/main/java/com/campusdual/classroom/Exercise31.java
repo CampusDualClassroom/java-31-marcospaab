76d8940e9e92729b7727e733c984fb0186809864
marcos.padin@outlook.es <you@example.com>
2024-09-30 13:16:45 +0200

    Tarea 31 realizada

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,37 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exercise31 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        String cadena="";
 
+
+        FileReader fr = null;
+        try {
+            fr = new FileReader("src/main/resources/lorem.txt");
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader b = new BufferedReader(fr);
+        while(true){
+            try {
+                if (!((cadena = b.readLine())!=null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(cadena);
+        }
+        try {
+            b.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
